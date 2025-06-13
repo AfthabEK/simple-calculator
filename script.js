e0430a70b1c4a3f47b6a536331f5c260337c1f6a
AfthabEK <mohamedafthabek@gmail.com>
2025-06-13 15:53:16 +0530

    Completed Calculator Logic
    Removed decimal button, instead added a backspace button
    The calculator logic follows the BODMAS rule

--- script.js
+++ script.js
@@ -3,30 +3,162 @@
 let numButtons = document.querySelectorAll(".num-pad > .btn")
 numButtons = Array.from(numButtons)
 const equalButton = numButtons.pop()
-const decimalButton = numButtons.pop()
+const backspaceButton = numButtons.pop()
 
-// numButtons.forEach(button => console.log(button.textContent));
 
 let opButtons = document.querySelectorAll(".op-pad > .btn")
 opButtons = Array.from(opButtons)
 
-// opButtons.forEach(button => console.log(button.textContent))
 
+const outputConsole = document.querySelector(".output-container")
+
+
+function evaluateExpression(exp) {
+
+    const terms = exp.split(" ")
+
+    console.log(terms);
+    
+    // let result = terms.reduce((sum,term) => {
+    //     return ['+','-','*','/'].includes(term)? sum:sum+parseInt(term);
+    // },0)
+/*
+
+    multiply
+    writeIndex = 0, readIndex = 0
+    if  arr[readindex] is number:
+        readindex ++ 
+    else if arr[readindex] is *
+        arr[writeIndex] * arr[readindex + 1]
+    else
+        readIndex ++
+        writeIndex = readindex
+    
+*/
+
+    let limit = terms.length
+    let writeIndex = 0
+    let readIndex = 0;
+
+    while(readIndex < limit) {
+        if(terms[readIndex] === '/') {
+            terms[writeIndex] = parseFloat(terms[writeIndex]) / parseFloat(terms[readIndex + 1])
+            terms[writeIndex].toFixed(5)
+            readIndex++;
+        }
+        else if (['+','-','*'].includes(terms[readIndex])) {
+            readIndex++;
+            writeIndex = readIndex
+        }
+        else {
+            readIndex++;
+        }
+
+    }
+
+    limit = writeIndex
+    writeIndex = 0
+    readIndex = 0;
+
+    while(readIndex < limit) {
+        if(terms[readIndex] === '*') {
+            terms[writeIndex] = parseFloat(terms[writeIndex]) * parseFloat(terms[readIndex + 1])
+            terms[writeIndex].toFixed(5)
+            readIndex++;
+        }
+        else if (['+','-'].includes(terms[readIndex])) {
+            readIndex++;
+            writeIndex = readIndex
+        }
+        else {
+            readIndex++;
+        }
+
+    }
+    
+    limit = writeIndex
+    writeIndex = 0
+    readIndex = 0;
+
+    while(readIndex < limit) {
+        if(terms[readIndex] === '+') {
+            terms[writeIndex] = parseFloat(terms[writeIndex]) + parseFloat(terms[readIndex + 1])
+            terms[writeIndex].toFixed(5)
+            readIndex++;
+        }
+        else if (['-'].includes(terms[readIndex])) {
+            readIndex++;
+            writeIndex = readIndex
+        }
+        else {
+            readIndex++;
+        }
+    }
+    limit = writeIndex
+    writeIndex = 0
+    readIndex = 0;
+
+    while(readIndex < limit) {
+        if(terms[readIndex] === '-') {
+            terms[writeIndex] = parseFloat(terms[writeIndex]) - parseFloat(terms[readIndex + 1])
+            terms[writeIndex]
+            readIndex++;
+        }
+        else {
+            readIndex++;
+        }
+
+    }
+
+    console.log(terms);
+
+    let result = terms[0]
+    return result;
+}
 
 function numButtonClicked(buttonText) {
-    alert(buttonText)
+    let currentText = outputConsole.textContent;
+    currentText += buttonText;
+    outputConsole.textContent = currentText;
 }
 
 function opButtonClicked(buttonText) {
-    alert(buttonText)
+    
+    let currentText = outputConsole.textContent;
+    const trimmedText = currentText.trim();
+    if(trimmedText == "" || 
+        ['+','-','*','/'].includes(trimmedText.trim().charAt(trimmedText.length - 1))
+    ) {
+        alert("Invalid Expression")
+        return
+    }
+
+    currentText = currentText + " " + buttonText + " ";
+    outputConsole.textContent = currentText;
 }
 
 function equalButtonClicked() {
-    alert("=")
+    let currentText = outputConsole.textContent;
+    const trimmedText = currentText.trim();
+        if(trimmedText == "" || 
+        ['+','-','*','/'].includes(trimmedText.trim().charAt(trimmedText.length - 1))
+    ) {
+        alert("Invalid Expression")
+        return
+    }
+
+    
+    let result  = evaluateExpression(trimmedText)
+    outputConsole.textContent = result.toFixed(5)
+    
 }
 
-function decimalButtonClicked() {
-    alert(".")
+function backspaceButtonClicked() {
+    let currentText = outputConsole.textContent;
+    let trimmedText = currentText.trim();
+    trimmedText = trimmedText.substring(0,trimmedText.length -1)
+    trimmedText = trimmedText.trim()
+    outputConsole.textContent = trimmedText
 }
 
 
@@ -39,5 +171,12 @@ numButtons.forEach(button =>
 opButtons.forEach(button => 
     button.addEventListener("click", ()=> opButtonClicked(button.textContent)))
 
-equalButton.addEventListener("click",() => equalButtonClicked(equalButton.textContent))
+
+// // Function reference 1: Used when we have parameters
+// equalButton.addEventListener("click",(e) => equalButtonClicked(e.target.textContent))
+
+equalButton.addEventListener("click",equalButtonClicked)
+
+// Function reference 2: Used when we don't have parameters
+backspaceButton.addEventListener("click",backspaceButtonClicked)
 
